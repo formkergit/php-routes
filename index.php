030262115cb4f9a87997f9c1b6228fc4a0421030
duser <user@email.fr>
2026-01-06 12:02:54 +0100

    Router indepedant la fonction redirect

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
-include 'fonctions.php';
+//include 'fonctions.php';
 require 'routes.php';
 
 //$page = isset($_GET['page']) ? $_GET['page'] : 'home';
 //$page = $_GET['page'] ?? 'home';
 $pageFiltre = filter_input(INPUT_GET, 'page' , FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $page = $pageFiltre ?? 'home';
-
-dg($page);
 
 if (!array_key_exists($page,$routes)) {
     redirect('404.php');
