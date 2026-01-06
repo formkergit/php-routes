80c396713b7d987627261c4592e5fb1e42823fda
duser <user@email.fr>
2026-01-06 09:16:56 +0100

    Initialisation du projet

---
+++ 404.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Non trouvé</title>
+</head>
+<body>
+    <h1>404 !</h1>
+    <p>Le document que vous demandé n'est pas trouvé !</p>
+</body>
+</html>
